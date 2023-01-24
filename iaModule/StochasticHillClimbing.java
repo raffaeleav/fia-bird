c03ea12ad8ad4e161dfa13c0c8112c84ac47b737
raffaeleav <raff.aviello@gmail.com>
2023-01-25 00:21:13 +0100

    chore: stesura iniziale dell' algoritmo

--- iaModule/StochasticHillClimbing.java
+++ iaModule/StochasticHillClimbing.java
@@ -19,18 +19,15 @@ public class StochasticHillClimbing {
     public static void stochasticClimbHill() throws AWTException {
         Utils utils = new Utils();
         Robot robot = new Robot();
-        robot.setAutoDelay(20 * 9);
 
         while(true){
             Rectangle futureBirdPosition = FlappyBird.flappyBird.bird,
                     actualBirdPosition = (Rectangle) FlappyBird.flappyBird.bird.clone();
             ArrayList<Rectangle> colums = FlappyBird.flappyBird.columns;
 
-            // se si perde la partita finisce
             if(FlappyBird.flappyBird.gameOver)
-                System.exit(0);
+               System.exit(0);
 
-            // punteggio per calcolare di quale pipe calcolare il punto ideale di salto
             int score = FlappyBird.flappyBird.score;
 
             // caso in cui il vicino e' peggio dello stato attuale (nel caso in cui non abbia saltato)
@@ -39,7 +36,6 @@ public class StochasticHillClimbing {
             if(utils.distanceObjectiveFunction(colums.get((score * 2) % 8), actualBirdPosition)
                     <= utils.distanceObjectiveFunction(colums.get((score * 2) % 8), futureBirdPosition)) {
                 utils.rewindBirdNoJump();
-                System.out.println("Renderizzo non salto");
             }
 
             // caso in cui il vicino e' peggio dello stato attuale (nel caso in cui abbia saltato)
@@ -47,15 +43,13 @@ public class StochasticHillClimbing {
             if(utils.distanceObjectiveFunction(colums.get((score * 2) % 8), actualBirdPosition)
                     <= utils.distanceObjectiveFunction(colums.get((score * 2) % 8), futureBirdPosition)) {
                 utils.rewindBirdJump();
-                System.out.println("Renderizzo salto");
             }
 
-            // l' algoritmo decide di saltare
             if(utils.jumpObjectiveFunction(colums.get((score * 2) % 8), futureBirdPosition)){
                 robot.keyRelease(KeyEvent.VK_SPACE);
+                robot.delay(20 * 9);
             }
 
-            // l' algoritmo decide di non saltare
             else {
                 robot.delay(20 * 9);
             }
